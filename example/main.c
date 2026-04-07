aa0feff9481432a7e47c04419646ecc0fb54589f
Ryzee119 <wendland@live.com.au>
2026-04-07 20:39:29 +0930

    Example: Rework example to test other SDL libraries

--- example/main.c
+++ example/main.c
@@ -1,154 +1,147 @@
-/* affine-textures.c ... */
-
-/*
-* This example creates an SDL window and renderer, and then draws a cube
-* using affine-transformed textures every frame.
-*
-* This code is public domain. Feel free to use it for any purpose!
-*/
-
-#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3_image/SDL_image.h>
 
-/* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
-static SDL_Texture *texture = NULL;
-static int texture_width = 0;
-static int texture_height = 0;
+static SDL_Texture *image_texture = NULL;
+static SDL_Texture *text_texture = NULL;
+static MIX_Mixer *mixer = NULL;
+static MIX_Track *track = NULL;
+static MIX_Audio *audio = NULL;
 
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
-
-#include <windows.h>
-void SDL_LogOutputDebugString(void *userdata, int category, SDL_LogPriority priority, const char *message) {
-    (void)userdata;
-    (void)category;
-    (void)priority;
-    DbgPrint("%s\n", message);
-}
-
-/* This function runs once at startup. */
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    SDL_Surface *surface = NULL;
-    char *bmp_path = NULL;
+    char path[256];
+    const char *base_path = SDL_GetBasePath();
 
-    SDL_SetLogOutputFunction(SDL_LogOutputDebugString, NULL);
-
-    SDL_SetAppMetadata("Example Renderer Affine Textures", "1.0", "com.example.renderer-affine-textures");
-
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO))
+    {
+        SDL_Log("SDL_Init Error: %s", SDL_GetError());
+        return 1;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/renderer/affine-textures", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("example", 640, 480, SDL_WINDOW_FULLSCREEN, &window, &renderer))
+    {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    /* Textures are pixel data that we upload to the video hardware for fast drawing. Lots of 2D
-       engines refer to these as "sprites." We'll do a static texture (upload once, draw many
-       times) with data from a bitmap file. */
-
-    /* SDL_Surface is pixel data the CPU can access. SDL_Texture is pixel data the GPU can access.
-       Load a .bmp into a surface, move it to a texture from there. */
-    SDL_asprintf(&bmp_path, "%sassets\\sample.bmp", SDL_GetBasePath());  /* allocate a string of the full file path */
-    surface = SDL_LoadBMP(bmp_path);
-    if (!surface) {
-        SDL_Log("Couldn't load bitmap: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
-
-    SDL_free(bmp_path);  /* done with this, the file is loaded. */
-
-    texture_width = surface->w;
-    texture_height = surface->h;
-
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (!texture) {
-        SDL_Log("Couldn't create static texture: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
-
-    SDL_DestroySurface(surface);  /* done with this, the texture has a copy of the pixels now. */
-
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
-}
-
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
-    if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
-    }
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
-}
-
-/* This function runs once per frame, and is the heart of the program. */
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    const float x0 = 0.5f * WINDOW_WIDTH;
-    const float y0 = 0.5f * WINDOW_HEIGHT;
-    const float px = SDL_min(WINDOW_WIDTH, WINDOW_HEIGHT) / SDL_sqrtf(3.0f);
-
-    const Uint64 now = SDL_GetTicks();
-    const float rad = (((float) ((int) (now % 2000))) / 2000.0f) * SDL_PI_F * 2;
-    const float cos = SDL_cosf(rad);
-    const float sin = SDL_sinf(rad);
-    const float k[3] = { 3.0f / SDL_sqrtf(50.0f), 4.0f / SDL_sqrtf(50.0f), 5.0f / SDL_sqrtf(50.0f)};
-    float mat[9] = { 
-         cos      + (1.0f-cos)*k[0]*k[0], -sin*k[2] + (1.0f-cos)*k[0]*k[1],  sin*k[1] + (1.0f-cos)*k[0]*k[2], 
-         sin*k[2] + (1.0f-cos)*k[0]*k[1],  cos      + (1.0f-cos)*k[1]*k[1], -sin*k[0] + (1.0f-cos)*k[1]*k[2], 
-        -sin*k[1] + (1.0f-cos)*k[0]*k[2],  sin*k[0] + (1.0f-cos)*k[1]*k[2],  cos      + (1.0f-cos)*k[2]*k[2],
-    };
-
-    float corners[16];
-    int i;
-
-    for (i = 0; i < 8; i++) {
-        const float x = (i & 1) ? -0.5f : 0.5f;
-        const float y = (i & 2) ? -0.5f : 0.5f;
-        const float z = (i & 4) ? -0.5f : 0.5f;
-        corners[0 + 2*i] = mat[0]*x + mat[1]*y + mat[2]*z;
-        corners[1 + 2*i] = mat[3]*x + mat[4]*y + mat[5]*z;
-    }
-
-    SDL_SetRenderDrawColor(renderer, 0x42, 0x87, 0xf5, SDL_ALPHA_OPAQUE);  // light blue background.
-    SDL_RenderClear(renderer);
-
-    for (i = 1; i < 7; i++) {
-        const int dir = 3 & ((i & 4) ? ~i : i);
-        const int odd = (i & 1) ^ ((i & 2) >> 1) ^ ((i & 4) >> 2);
-        if (0 < (odd ? 1.0f : -1.0f) * mat[5 + dir]) continue;
-        int origin_index = (1 << ((dir - 1) % 3));
-        int right_index = (1 << ((dir + odd) % 3)) | origin_index;
-        int down_index = (1 << ((dir + (odd^1)) % 3)) | origin_index;
-        if (!odd) {
-            origin_index ^= 7;
-            right_index ^= 7;
-            down_index ^= 7;
+    // SDL_Image test
+    {
+        SDL_snprintf(path, sizeof(path), "%sassets\\sample.jpg", base_path);
+        SDL_Surface *image_surface = IMG_Load(path);
+        if (image_surface)
+        {
+            image_texture = SDL_CreateTextureFromSurface(renderer, image_surface);
+            SDL_DestroySurface(image_surface);
         }
-        SDL_FPoint origin, right, down;
-        origin.x = x0 + px*corners[0 + 2*origin_index];
-        origin.y = y0 + px*corners[1 + 2*origin_index];
-        right.x  = x0 + px*corners[0 + 2*right_index];
-        right.y  = y0 + px*corners[1 + 2*right_index];
-        down.x   = x0 + px*corners[0 + 2*down_index];
-        down.y   = y0 + px*corners[1 + 2*down_index];
-        SDL_RenderTextureAffine(renderer, texture, NULL, &origin, &right, &down);
     }
 
-    SDL_RenderPresent(renderer);
+    // SDL_TTF test
+    {
+        if (!TTF_Init())
+        {
+            SDL_Log("Couldn't initialize SDL_ttf: %s", SDL_GetError());
+            return SDL_APP_FAILURE;
+        }
+        SDL_snprintf(path, sizeof(path), "%sassets\\Roboto-Regular.ttf", base_path);
+        TTF_Font *font = TTF_OpenFont(path, 24);
+        if (font)
+        {
+            SDL_Color white = {255, 255, 255, 255};
+            SDL_Surface *ttf_surface = TTF_RenderText_Blended(font, "SDL3 - nxdk!", 0, (SDL_Color){255, 255, 255, 255});
+            if (ttf_surface)
+            {
+                text_texture = SDL_CreateTextureFromSurface(renderer, ttf_surface);
+            }
+            SDL_DestroySurface(ttf_surface);
+            TTF_CloseFont(font);
+        }
+        TTF_Quit();
+    }
 
-    return SDL_APP_CONTINUE;
-}
+    // SDL_Mixer
+    {
+        SDL_snprintf(path, sizeof(path), "%sassets\\music.mp3", base_path);
+        MIX_Init();
+        mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+        audio = MIX_LoadAudio(mixer, path, false);
+        track = MIX_CreateTrack(mixer);
+        MIX_SetTrackAudio(track, audio);
+        MIX_PlayTrack(track, 0);
+    }
 
-/* This function runs once at shutdown. */
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
-    SDL_DestroyTexture(texture);
-    /* SDL will clean up the window/renderer for us. */
+    while (1)
+    {
+        SDL_SetRenderDrawColor(renderer, 33, 33, 33, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+
+        // Test primitive rendering functions
+        {
+            // Draw a primitive rectangle to the screen.
+            const SDL_FRect primitive_rect = {
+                .x = 640 / 2 - 150,
+                .y = 480 / 2 - 150,
+                .w = 300,
+                .h = 300};
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
+            SDL_RenderFillRect(renderer, &primitive_rect);
+
+            // Sprinkle some points
+            SDL_FPoint points[128];
+            for (size_t i = 0; i < SDL_arraysize(points); ++i)
+            {
+                points[i].x = SDL_randf() * 640;
+                points[i].y = SDL_randf() * 480;
+            }
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE); 
+            SDL_RenderPoints(renderer, points, SDL_arraysize(points));
+
+            // Draw a line
+            SDL_SetRenderDrawColor(renderer, 255, 255, 0, SDL_ALPHA_OPAQUE);
+            SDL_RenderLine(renderer, 0, 0, 640, 480);
+            SDL_RenderLine(renderer, 0, 480, 640, 0);
+        }
+
+        // Draw an image using the texture created by SDL_image
+        if (image_texture)
+        {
+            const SDL_FRect image_rect = {
+                .x = 640 / 2 - 100,
+                .y = 480 / 2 - 75,
+                .w = 200,
+                .h = 150};
+            SDL_RenderTexture(renderer, image_texture, NULL, &image_rect);
+        }
+
+        // Draw some text using the texture created by SDL_ttf
+        if (text_texture)
+        {
+            const SDL_FRect text_rect = {
+                .x = 640 / 2 - 100,
+                .y = 480 / 2 + 100,
+                .w = 200,
+                .h = 50};
+            SDL_RenderTexture(renderer, text_texture, NULL, &text_rect);
+        }
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1000);
+    }
+
+    MIX_StopAllTracks(mixer, 0);
+    MIX_DestroyTrack(track);
+    MIX_DestroyAudio(audio);
+    MIX_Quit();
+
+    SDL_DestroyTexture(image_texture);
+    SDL_DestroyTexture(text_texture);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
